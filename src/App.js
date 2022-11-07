aed602e7a344303b142ba207a8deac06d3c56a1d
ShuumaIwamoto <iwa06ki12da@gmail.com>
2022-11-08 00:50:51 +0900

    testでthree.jsを入れて練習

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import { Image } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Image, Scroll, ScrollControls, useScroll } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useRef } from "react";
 import "./App.css";
 
 function Images() {
+  const { height } = useThree((state) => state.viewport);
+  const group = useRef();
+  const data = useScroll();
+
+  useFrame(() => {
+    group.current.children[0].material.zoom = 1 + data.range(0, 1 / 3) / 3;
+    group.current.children[1].material.zoom = 1 + data.range(0, 1 / 3) / 3;
+    group.current.children[2].material.zoom = 1 + data.range(0, 1 / 3) / 3;
+    group.current.children[3].material.zoom = 1 + data.range(0, 1 / 3) / 3;
+  });
+
   return (
-    <group>
-      <Image url="./images/shop1.jpg" scale={[3, 2, 1]} position={[-1, 0, 1]} />
+    <group ref={group}>
+      <Image
+        url="./images/shop1.jpg"
+        scale={[4, height, 1]}
+        position={[-1, 0, 1]}
+      />
+      <Image url="./images/shop2.jpg" scale={3} position={[2, 0, 1]} />
+      <Image
+        url="./images/shop3.jpg"
+        scale={[1, 3.5, 1]}
+        position={[-2.3, -height, 2]}
+      />
+      <Image
+        url="./images/shop4.jpg"
+        scale={[1, 2.7, 1]}
+        position={[-1.4, -height - 0.7, 1]}
+      />
     </group>
   );
 }
@@ -13,7 +40,19 @@ function Images() {
 function App() {
   return (
     <Canvas>
-      <Images />
+      <ScrollControls pages={2} damping={3} horizontal={false}>
+        <Scroll>
+          <Images />
+        </Scroll>
+        <Scroll html>
+          <h1 style={{ position: "absolute", top: "60vh", left: "1.5em" }}>
+            SAMPLE
+          </h1>
+          <h1 style={{ position: "absolute", top: "140vh", left: "40vw" }}>
+            ThreeJS
+          </h1>
+        </Scroll>
+      </ScrollControls>
     </Canvas>
   );
 }
